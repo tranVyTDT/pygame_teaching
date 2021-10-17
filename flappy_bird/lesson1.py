14aba661c96e4e6ddce664265be4fa61f3ca1636
tranVyTDT <40004136+tranVyTDT@users.noreply.github.com>
2021-10-18 02:05:53 +0700

    add lesson 2

--- flappy_bird/lesson1.py
+++ flappy_bird/lesson1.py
@@ -1,28 +1,34 @@
 import pygame
 from pygame.locals import *
-from sys import exit
+from sys import exit, flags
+
 
 SCREEN_HEIGHT = 550
 SCREEN_WIDTH = 288
 
 
 background_image_filename = r'assets\background-day.png'
-mouse_image_filename = r'assets\yellowbird-midflap.png'
+message_filename = r'assets\message.png'
 
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT), flags=RESIZABLE, depth=32)
 pygame.display.set_caption("Hello, World!")
 
 background = pygame.image.load(background_image_filename).convert()
-mouse_cursor = pygame.image.load(mouse_image_filename).convert_alpha()
+message = pygame.image.load(message_filename).convert_alpha()
+
+x = SCREEN_WIDTH / 2 - message.get_width() / 2
+y = SCREEN_HEIGHT / 2 - message.get_height() / 2
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+        
+        print(event)
 
     screen.blit(background, (0,0))
-    screen.blit(mouse_cursor, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    screen.blit(message, (x, y))
 
     pygame.display.update()
