e7d5112838e449d8fd9592f884f24a54a4ba86b3
tranVyTDT <40004136+tranVyTDT@users.noreply.github.com>
2021-12-24 16:43:17 +0700

    zombie collide with walls

---
+++ zombie_shooter/lesson_4/part_6_zombie_collide_with_wall/settings.py
@@ -0,0 +1,34 @@
+import pygame as pg
+
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# game settings
+WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
+FPS = 60
+TITLE = "Tilemap Demo"
+BGCOLOR = DARKGREY
+
+TILESIZE = 64
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+WALL_IMG = r'PNG\Tiles\tile_196.png'
+
+# player settings
+PLAYER_SPEED = 300
+PLAYER_ROT_SPEED = 2.5
+PLAYER_IMG = 'PNG\Hitman 1\hitman1_gun.png'
+PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
+
+# mob settings
+MOB_IMG = 'PNG\Zombie 1\zoimbie1_hold.png'
+MOD_SPEED = 150
+MOD_HIT_RECT = pg.Rect(0, 0, 30, 30)
