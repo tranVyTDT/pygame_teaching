40ab28249181e987a2a410bf91bcf4c26492de00
tranVyTDT <40004136+tranVyTDT@users.noreply.github.com>
2022-05-18 16:21:29 +0700

    lesson 9

---
+++ zombie_shooter/lesson_9/part_1_tweening_and_easing/settings.py
@@ -0,0 +1,66 @@
+import pygame as pg
+vec = pg.math.Vector2
+
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# game settings
+WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
+FPS = 60
+TITLE = "Tilemap Demo"
+BGCOLOR = DARKGREY
+
+TILESIZE = 64
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+WALL_IMG = r'PNG\Tiles\tile_196.png'
+
+# player settings
+PLAYER_SPEED = 300
+PLAYER_ROT_SPEED = 2.5
+PLAYER_IMG = r'PNG\Hitman 1\hitman1_gun.png'
+PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
+BARREL_OFFSET = vec(30, 10)
+PLAYER_HEALTH = 100
+
+
+# mob settings
+MOB_IMG = r'PNG\Zombie 1\zoimbie1_hold.png'
+MOD_SPEED = [125, 150, 175, 200]
+MOD_HIT_RECT = pg.Rect(0, 0, 30, 30)
+MOB_HEALTH = 100
+MOB_DAMAGE = 10
+MOB_KNOCKBACK = 20
+AVOID_RADIUS = 50
+DETECT_RADIUS = 400
+
+
+# gun settings
+BULLET_IMG = r'PNG\bullet.png'
+BULLET_SPEED = 600
+BULLET_LIFETIME = 1000
+BULLET_RATE = 150
+KICKBACK = 200
+GUN_SPREAD = 20
+BULLET_DAMAGE = 10
+
+# Effects
+MUZZLE_FLASHES = ['whitePuff15.png', 'whitePuff16.png', 'whitePuff17.png',
+                  'whitePuff18.png']
+FLASH_DURATION = 40
+
+
+ITEM_IMAGES = {'health': 'health_pack.png'}
+HEALTH_PACK_AMOUNT = 20
+BOB_RANGE = 15
+BOB_SPEED = 0.4
+
+
---
+++ zombie_shooter/lesson_9/part_2_sound/main.py
@@ -0,0 +1,177 @@
+import pygame as pg
+from os import path
+import sys
+from settings import *
+from sprites import *
+from tilemap import *
+
+def draw_player_health(surf, x, y, pct):
+    if pct < 0:
+        pct = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 20
+    fill = pct * BAR_LENGTH
+    outline_rect = pg.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pg.Rect(x, y, fill, BAR_HEIGHT)
+    if pct > 0.6:
+        col = GREEN
+    elif pct > 0.3:
+        col = YELLOW
+    else:
+        col = RED
+    pg.draw.rect(surf, col, fill_rect)
+    pg.draw.rect(surf, WHITE, outline_rect, 2)
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        self.load_data()
+
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        assets_folder = path.join(game_folder, '../../assets')
+        snd_folder = path.join(game_folder, '../../snd')
+        music_folder = path.join(game_folder, '../../music')
+        map_folder = path.join(game_folder, 'maps')
+        self.map = TiledMap(path.join(map_folder, 'level1.tmx'))
+        self.map_img = self.map.make_map()
+        self.map_rect = self.map_img.get_rect()
+        self.player_img = pg.image.load(path.join(assets_folder, PLAYER_IMG)).convert_alpha()
+        self.bullet_img = pg.image.load(path.join(assets_folder, BULLET_IMG)).convert_alpha()
+        self.mob_img = pg.image.load(path.join(assets_folder, MOB_IMG)).convert_alpha()
+        self.wall_img = pg.image.load(path.join(assets_folder, WALL_IMG)).convert_alpha()
+        self.wall_img = pg.transform.scale(self.wall_img, (TILESIZE, TILESIZE))
+        self.gun_flashes = []
+        for img in MUZZLE_FLASHES:
+            self.gun_flashes.append(pg.image.load(path.join(assets_folder, img)).convert_alpha())
+        self.item_images = {}
+        for item in ITEM_IMAGES:
+            self.item_images[item] = pg.image.load(path.join(assets_folder, ITEM_IMAGES[item])).convert_alpha()
+        pg.mixer.music.load(path.join(music_folder, BG_MUSIC))
+        self.effects_sounds = {}
+        for type in EFFECTS_SOUNDS:
+            self.effects_sounds[type] = pg.mixer.Sound(path.join(snd_folder, EFFECTS_SOUNDS[type]))
+        self.weapon_sounds = {}
+        self.weapon_sounds['gun'] = []
+        for snd in WEAPON_SOUNDS_GUN:
+            self.weapon_sounds['gun'].append(pg.mixer.Sound(path.join(snd_folder, snd)))
+        self.zombie_moan_sounds = []
+        for snd in ZOMBIE_MOAN_SOUNDS:
+            s = pg.mixer.Sound(path.join(snd_folder, snd))
+            s.set_volume(0.1)
+            self.zombie_moan_sounds.append(s)
+        self.player_hit_sounds = []
+        for snd in PLAYER_HIT_SOUNDS:
+            self.player_hit_sounds.append(pg.mixer.Sound(path.join(snd_folder, snd)))
+        self.sombie_hit_sounds = []
+        for snd in ZOMBIE_HIT_SOUNDS:
+            self.player_hit_sounds.append(pg.mixer.Sound(path.join(snd_folder, snd)))
+
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.mobs = pg.sprite.Group()
+        self.bullets = pg.sprite.Group()
+        self.items = pg.sprite.Group()
+        self.camera = Camera(self.map.width, self.map.height)
+        
+        for tile_object in self.map.tmxdata.objects:
+            obj_center = vec(tile_object.x + tile_object.width / 2,
+                    tile_object.y + tile_object.height / 2)
+            if tile_object.name == 'player':
+                self.player = Player(self, obj_center.x, obj_center.y)
+            if tile_object.name == 'zombie':
+                Mob(self, obj_center.x, obj_center.y)
+            if tile_object.name == 'wall':
+                Obstacle(self, obj_center.x, obj_center.y,
+                         tile_object.width, tile_object.height)
+            if tile_object.name in ['health']:
+                Item(self, obj_center, tile_object.name)
+        
+        self.effects_sounds['level_start'].play()
+
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        pg.mixer.music.play(loops=-1)
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+        self.camera.update(self.player)
+        # player hits items
+        hits = pg.sprite.spritecollide(self.player, self.items, False)
+        for hit in hits:
+            if random.random() < 0.7:
+                random.choice(self.player_hit_sounds).play()
+            if hit.type == 'health' and self.player.health < PLAYER_HEALTH:
+                hit.kill()
+                self.effects_sounds['health_up'].play()
+                self.player.add_health(HEALTH_PACK_AMOUNT)
+        # zombie hit player
+        hits = pg.sprite.spritecollide(self.player, self.mobs, False, collide_hit_rect)
+        for hit in hits:
+            self.player.health -= MOB_DAMAGE
+            hit.vec = vec(0, 0) # làm zombie khựng lại
+            if self.player.health <= 0:
+                self.playing = False
+        if hits: # player sẽ bị đẩy ra 1 xíu khi chạm zombie
+            self.player.pos += vec(MOB_KNOCKBACK, 0).rotate(-hits[0].rot)
+
+        # bullet collide mobs
+        zombies = pg.sprite.groupcollide(self.mobs, self.bullets, False, True) 
+        for zombie in zombies:
+            zombie.health -= BULLET_DAMAGE
+            zombie.vec = vec(0, 0)
+
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        pg.display.set_caption(f"{self.clock.get_fps():.2f}")
+        self.screen.blit(self.map_img, self.camera.apply_rect(self.map_rect))
+        for sprite in self.all_sprites:
+            if isinstance(sprite, Mob):
+                sprite.draw_health()
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+        draw_player_health(self.screen, 10, 10, self.player.health / PLAYER_HEALTH)
+        pg.display.flip()
+
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
